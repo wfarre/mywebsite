330347c9e015bddd8c5df3edb4ae4139cb35c1d7
wfarre <76807429+wfarre@users.noreply.github.com>
2021-05-10 22:43:28 +0800

    mail form set up

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+
 const portfolioArray = document.querySelectorAll(".website-img");
+
+const userID = process.env.USER_ID;
+
+
 
 
 portfolioArray.forEach(function(img){
@@ -23,4 +28,26 @@ crosses.forEach(function(cross){
         document.querySelector(".overlay").style.display = "none";
     })
 
+});
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+   console.log(event);
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_146w2nx';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
 });
